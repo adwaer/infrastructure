96e9e282d4cb11bb47f40e965d1b5f445b52e366
ADWAER-PC\Adwaer <adwaer@live.ru>
2017-05-30 01:33:22 +0300

    accept custom userstore

--- In.Identity/Config/IdentityConfig.cs
+++ In.Identity/Config/IdentityConfig.cs
@@ -10,6 +10,29 @@ namespace In.Identity.Config
 {
     public static class IdentityConfig<T, TRole> where T : IdentityUser<Guid, IdentityUserLogin<Guid>, TRole, IdentityUserClaim<Guid>> where TRole : IdentityUserRole<Guid>, new()
     {
+        public static void Ioc<TStore>(ContainerBuilder builder)
+        {
+            builder.RegisterType<TStore>()
+                .As<IUserStore<T, Guid>>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<IdentityUserManager<T>>()
+                .As<UserManager<T, Guid>>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<SignInManager<T, Guid>>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication)
+                .InstancePerLifetimeScope();
+
+            builder.RegisterApiControllers(typeof(IdentityConfig<T, TRole>).Assembly);
+        }
+
+
         public static void Ioc(ContainerBuilder builder)
         {
             builder.RegisterType<IdentityUserStore<T, TRole>>()
