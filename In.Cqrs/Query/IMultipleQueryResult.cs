6d4943ba81b565d4278d3a123d0b5e8a7f4dd5d8
Azat <adwaer@live.ru>
2017-07-07 12:58:44 +0300

    multipl/single query results

---
+++ In.Cqrs/Query/IMultipleQueryResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In.Cqrs.Query
+{
+    public interface IMultipleQueryResult<T>
+    {
+        T[] Data { get; set; }
+    }
+
+    public class MultipleQueryResult<T> : IMultipleQueryResult<T>
+    {
+        public T[] Data { get; set; }
+    }
+}
---
+++ In.Cqrs/Query/ISingleQueryResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In.Cqrs.Query
+{
+    public interface ISingleQueryResult<T>
+    {
+        T Data { get; set; }
+    }
+
+    public class SingleQueryResult<T> : ISingleQueryResult<T>
+    {
+        public T Data { get; set; }
+    }
+}
